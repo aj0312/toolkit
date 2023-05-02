d3689068ed811956d449b54578e18a565eb87863
Anish <chikodianish@gmail.com>
2023-05-02 09:29:04 +0530

    removed randomString() parameter

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/aj0312/go-mod-toolkit/v2
+
+go 1.19
---
+++ v2/tools.go
@@ -0,0 +1,19 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789+_"
+
+type Tools struct {
+}
+
+func (t *Tools) RandomString() string {
+	n := 10
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
